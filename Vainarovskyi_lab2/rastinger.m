e80e588925e0b1824636f8a78c9253dff6ed90ff
Vainarovskiy_Edgar <w3st2008@ya.ru>
2015-06-11 18:22:06 +0300

    lab2

---
+++ Vainarovskyi_lab2/rastinger.m
@@ -0,0 +1,3 @@
+function y = rastinger(x) %функція Растригина 
+y = 10 + x.^2-10*cos(2*pi*x);
+end
---
+++ Vainarovskyi_lab2/schwefel.m
@@ -0,0 +1,4 @@
+function y = schwefel(x) %функція Швефеля
+y = 418.9829 -x(1).*sin(sqrt(abs(x(1))))-x(2).*sin(sqrt(abs(x(2))));
+end
+
